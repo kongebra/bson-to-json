4a55cf6ab733c688e8f45d6a8efdd6168e4c63a4
Svein Are Danielsen <svein.danielsen@inmeta.no>
2019-12-23 11:30:31 +0100

    refactored tool to read binary object by object

--- main.go
+++ main.go
@@ -1,207 +1,135 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
+	"encoding/binary"
 	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
-	"os"
-	"strings"
 	"time"
 
-	// "github.com/globalsign/mgo/bson"
-	// "go.mongodb.org/mongo-driver/bson"
-	"github.com/mongodb/mongo-tools/bsondump"
+	"github.com/globalsign/mgo/bson"
 )
 
 var (
-	// ErrNotValidJSON error
-	ErrNotValidJSON = errors.New("json is not valid")
-)
-
-var (
-	input  string
-	output string
-	pretty bool
-	check  bool
-	debug  bool
+	sourceFile string
+	outputFile string
+	pretty     bool
+	debug      bool
 )
 
 func init() {
-	flag.StringVar(&input, "input", "", "(Required) Input file")
-	flag.StringVar(&input, "i", "", "(Required) Input file (shorthand)")
+	flag.StringVar(&sourceFile, "source", "", "source file (BSON)")
+	flag.StringVar(&outputFile, "output", "output.json", "output file (JSON)")
+	flag.BoolVar(&pretty, "pretty", false, "pretty output (indented)")
+	flag.BoolVar(&debug, "debug", false, "get debug output")
 
-	flag.StringVar(&output, "output", "data.json", "Output file default: 'data.json')")
-	flag.StringVar(&output, "o", "data.json", "Output file default: 'data.json') (shorthand)")
+	flag.Parse()
+}
 
-	flag.BoolVar(&pretty, "pretty", false, "Pretty output (default: false)")
-	flag.BoolVar(&pretty, "p", false, "Pretty output (default: false) (shorthand)")
+func writeFile(v interface{}) {
+	var data []byte
+	var err error
 
-	flag.BoolVar(&check, "check", false, "Check and validate BSON during processing (default: false)")
-	flag.BoolVar(&check, "c", false, "Check and validate BSON during processing (default: false) (shorthand)")
+	if pretty {
+		data, err = json.MarshalIndent(v, "", "\t")
+	} else {
+		data, err = json.Marshal(v)
+	}
 
-	flag.BoolVar(&debug, "debug", false, "debug")
-	flag.BoolVar(&debug, "d", false, "debug")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(outputFile, data, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
-	defer measure(time.Now(), "main")
-
-	// Parse flags
-	flag.Parse()
-	checkFlags()
-
-	err := BSON2JSON()
-	if err != nil {
-		log.Fatal(err)
+	// Measure time if we want debugging
+	if debug {
+		defer measure(
+			time.Now(),
+			fmt.Sprintf("Converting \"%s\" to JSON", sourceFile),
+		)
 	}
 
-	data, err := getOutputData()
+	var (
+		offset  int32
+		docSize int32
+		reader  io.Reader
+		buffer  *bytes.Buffer
+	)
+
+	// Result slice of BSON objects
+	result := make([]bson.M, 0)
+
+	// Read data from file
+	data, err := ioutil.ReadFile(sourceFile)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	valid := validateOutputData(data)
-	if !valid {
-		err := fix(data)
-		if err != nil {
-			log.Fatal(err)
+	// Run forever
+	for {
+		// Create new bytes reader based on offset
+		reader = bytes.NewReader(data[offset:])
+
+		// Read binary to bytes reader
+		if err := binary.Read(reader, binary.LittleEndian, &docSize); err != nil {
+			// Check if we have reached end of file
+			if err == io.EOF {
+				break
+			}
+
+			// Panic, get out!
+			panic(err)
 		}
-	}
-}
 
-func checkFlags() {
-	// Check if input is blank
-	if input == "" {
-		log.Fatal("flag needs an argument: -input")
+		// Min and max bson document sizes allowed
+		if docSize < 5 || docSize > 16777216 {
+			// Panic with message
+			panic("invalid document size")
+		}
+
+		// Create new bytes buffer with the capacity of the document size
+		buffer = bytes.NewBuffer(make([]byte, 0, docSize))
+
+		// Write binaries to the bytes buffer
+		if err := binary.Write(buffer, binary.LittleEndian, docSize); err != nil {
+			// Panic on the dance floor!
+			panic(err)
+		}
+
+		// Copy N size from reader to buffer
+		if _, err := io.CopyN(buffer, reader, int64(docSize-4)); err != nil {
+			panic(err)
+		}
+
+		// Declare document
+		var document bson.M
+
+		// Unmarshal bytes to an interface
+		if err := bson.Unmarshal(buffer.Bytes(), &document); err != nil {
+			panic(err)
+		}
+
+		// Append document to result
+		result = append(result, document)
+
+		// Add document size to offset
+		offset += docSize
 	}
 
-	// Check if input is a BSON document
-	if !strings.HasSuffix(input, ".bson") {
-		log.Fatal("file type error: input file needs to be a BSON document")
-	}
-
-	// Check if output is a JSON file
-	if !strings.HasSuffix(output, ".json") {
-		log.Fatal("file type error: output file ened to be a JSON file")
-	}
+	// Write result to file
+	writeFile(result)
 }
 
 func measure(start time.Time, text string) {
-	if debug {
-		fmt.Printf("%s took %s\n", text, time.Since(start))
-	}
-}
-
-// BSON2JSON func
-func BSON2JSON() error {
-	defer measure(time.Now(), "converting BSON to JSON")
-
-	// BSON dump options
-	options := bsondump.Options{
-		ToolOptions: nil,
-		OutputOptions: &bsondump.OutputOptions{
-			Type:         bsondump.JSONOutputType,
-			ObjCheck:     check,
-			Pretty:       pretty || true,
-			BSONFileName: input,
-			OutFileName:  output,
-		},
-	}
-
-	// Create BSON dump
-	bd, err := bsondump.New(options)
-	if err != nil {
-		return err
-	}
-	// Close up, eventually
-	defer bd.Close()
-
-	// Create new output file
-	file, err := os.Create(output)
-	if err != nil {
-		return err
-	}
-
-	// Close output file, eventually
-	defer file.Close()
-
-	// Set output file as output writer
-	bd.OutputWriter = file
-
-	// Convert to JSON
-	_, err = bd.JSON()
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func getOutputData() ([]byte, error) {
-	return ioutil.ReadFile(output)
-}
-
-func validateOutputData(data []byte) bool {
-	defer measure(time.Now(), "validate output JSON")
-
-	// Check if new data is valid
-	return json.Valid(data)
-}
-
-func fix(data []byte) error {
-	defer measure(time.Now(), "fix JSON")
-
-	bytesReader := bytes.NewReader(data)
-
-	// Set first to true
-	first := true
-
-	// Create a new scanner
-	scanner := bufio.NewScanner(bytesReader)
-
-	var sb strings.Builder
-	// Append a bracket on the start
-	sb.WriteString("[\n")
-
-	// Scan lines
-	for scanner.Scan() {
-		// Get text in line
-		line := scanner.Text()
-
-		// TODO (Svein): This does not work with non-pretty
-		// Check if line is equal to '{' and not the first line
-		if line == "{" && !first {
-			sb.WriteString(",\n")
-		}
-
-		// Write line
-		sb.WriteString(strings.ReplaceAll(line, "\t", ""))
-
-		// Set first line to false
-		if first {
-			first = false
-		}
-	}
-
-	// Write last line
-	sb.WriteString("\n]")
-
-	// Check for scanner errors
-	if err := scanner.Err(); err != nil {
-		return err
-	}
-
-	// Check if new data is valid
-	valid := json.Valid([]byte(sb.String()))
-	if !valid {
-		return ErrNotValidJSON
-	}
-
-	// Write to file
-	return ioutil.WriteFile(output, []byte(sb.String()), 0644)
+	fmt.Printf("%s took %s\n", text, time.Since(start))
 }
